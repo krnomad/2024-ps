06caa21c87bfab85ef5cdbb746eda5011140e237
krnomad <krnomad@gmail.com>
2024-07-09 21:46:01 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+    string a; cin >> a;
+    int n; cin >> n;
+    cout << a[n-1];
+    return 0;
+}
