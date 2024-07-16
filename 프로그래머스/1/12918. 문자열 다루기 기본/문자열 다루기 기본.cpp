c9258d1968243b1191ac258de74248e473d742e7
krnomad <krnomad@gmail.com>
2024-07-17 06:21:24 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+bool solution(string s) {
+    int len = s.size();
+    if ( len != 4 && len != 6 ) {
+        return false;
+    }
+    
+    for ( auto c : s ) {
+        if ( c < '0' || c > '9' ) return false;
+    }
+    
+    return true;
+}
